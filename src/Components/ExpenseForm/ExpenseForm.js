65df602f2dca59ad726856f93b8b793cbb15d120
Saif-09 <saifmd238@gmail.com>
2023-07-28 04:40:13 +0530

    issue resolved for useState error

--- src/Components/ExpenseForm/ExpenseForm.js
+++ src/Components/ExpenseForm/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 import styles from "./ExpenseForm.module.css";
 
 export default function ExpenseForm({ addExpense }) {
